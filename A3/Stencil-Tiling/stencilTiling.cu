90b7f70321a19584cba3e55da7491844b4d6252c
Rohit M P <rohitmp99@gmail.com>
2018-12-05 00:59:54 +0530

    Reverting

--- A3/Stencil-Tiling/stencilTiling.cu
+++ A3/Stencil-Tiling/stencilTiling.cu
@@ -21,34 +21,18 @@ using namespace std;
 
 __global__ void compute(float *deviceInputData, float *deviceOutputData, int width, int height, int depth){
     int x = blockDim.x*blockIdx.x + threadIdx.x;
-    int y = blockDim.y*blockIdx.y + threadIdx.y;    
+    int y = blockDim.y*blockIdx.y + threadIdx.y;
     int i = x/depth;
-    int j = x%depth;
-    int k = y;
-
-    __shared__ float shared_mem[ 34*34 ];
-
-    float val = 0.1;
-    if(i>=1 && i<height-1 && j>0 && j<width-1 && k>0 && k<depth-1){
-        int a,b,c;
-        for(a=-1; a<=1; a++){
-            for(b=-1; b<=1; b++){
-                for(c=-1; c<=1; c++){
-                    shared_mem[((i+a)*width + (j+b)) * depth + (k+c)] = deviceInputData[((i+a)*width + (j+b)) * depth + (k+c)];
-                }
-            }
-        }    
-        __syncthreads();
-        val = shared_mem[((i-1)*width + (j)) * depth + (k)] + shared_mem[((i)*width + (j-1)) * depth + (k)] + shared_mem[((i)*width + (j)) * depth + (k-1)]
-            + shared_mem[((i+1)*width + (j)) * depth + (k)] + shared_mem[((i)*width + (j+1)) * depth + (k)] + shared_mem[((i)*width + (j)) * depth + (k+1)]
-            - 6*shared_mem[((i)*width + (j)) * depth + (k)];
-        val=0.1;
+    int k = x%depth;
+    int j = y;
+    if(j>0 && j<height-1 && i>0 && i<width-1 && k>0 && k<depth-1){
+        float val = deviceInputData[((i-1)*width + (j)) * depth + (k)] + deviceInputData[((i)*width + (j-1)) * depth + (k)] 
+            + deviceInputData[((i)*width + (j)) * depth + (k-1)] + deviceInputData[((i+1)*width + (j)) * depth + (k)] 
+            + deviceInputData[((i)*width + (j+1)) * depth + (k)] + deviceInputData[((i)*width + (j)) * depth + (k+1)]
+            - 6*deviceInputData[((i)*width + (j)) * depth + (k)];
         deviceOutputData[((i)*width + (j)) * depth + (k)] = Clamp(val, 0.0, 1.0);
     }
-    val=0.15;
-    deviceOutputData[((i)*width + (j)) * depth + (k)] = Clamp(val, 0.0, 1.0);
 }
-
 
 static void launch_stencil(float *deviceOutputData, float *deviceInputData, 
     int width, int height, int depth) {
@@ -100,23 +84,6 @@ int main(int argc, char *argv[]) {
     cudaMemcpy(output.data, deviceOutputData, width * height * depth * sizeof(float),
         cudaMemcpyDeviceToHost);
     wbTime_stop(Copy, "Copying data from the GPU");
-
-    /*float *hostOutputData = (float*)malloc(sizeof(float)*width*height*depth);
-    cudaMemcpy(hostOutputData, deviceOutputData, width*height*depth*sizeof(float), cudaMemcpyDeviceToHost);
-    int i,j,k;
-    for(i=1; i<width-1; i++){
-        for(j=1; j<height-1; j++){
-            for(k=1; k<depth-1; k++){
-                if(hostOutputData[((i)*width + (j)) * depth + (k)] != hostInputData[((i-1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j-1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k-1)]
-                + hostInputData[((i+1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j+1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k+1)]
-                - 6*hostInputData[((i)*width + (j)) * depth + (k)])
-                printf("Failed at %d,%d,%d : Found:%f ; Expected:%f\n",i,j,k,hostOutputData[((i)*width + (j)) * depth + (k)], hostInputData[((i-1)*width + (j)) * depth + (k)] 
-                + hostInputData[((i)*width + (j-1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k-1)]
-                + hostInputData[((i+1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j+1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k+1)]
-                - 6*hostInputData[((i)*width + (j)) * depth + (k)]);
-            }
-        }
-    }*/
 
     wbSolution(arg, output);
 
