4625c3f15f1163766345d808b9dad330130c1ea7
Rohit M P <rohitmp99@gmail.com>
2018-12-04 22:11:40 +0530

    Fix Required

--- A3/Stencil-Tiling/stencilTiling.cu
+++ A3/Stencil-Tiling/stencilTiling.cu
@@ -21,8 +21,8 @@ using namespace std;
 
 __global__ void compute(float *deviceInputData, float *deviceOutputData, int width, int height, int depth){
     int x = blockDim.x*blockIdx.x + threadIdx.x;
-    int y = blockDim.y*blockIdx.y + threadIdx.y;
-    int i = x/width;
+    int y = blockDim.y*blockIdx.y + threadIdx.y;    
+    int i = x/depth;
     int j = x%depth;
     int k = y;
 
@@ -100,6 +100,23 @@ int main(int argc, char *argv[]) {
     cudaMemcpy(output.data, deviceOutputData, width * height * depth * sizeof(float),
         cudaMemcpyDeviceToHost);
     wbTime_stop(Copy, "Copying data from the GPU");
+
+    /*float *hostOutputData = (float*)malloc(sizeof(float)*width*height*depth);
+    cudaMemcpy(hostOutputData, deviceOutputData, width*height*depth*sizeof(float), cudaMemcpyDeviceToHost);
+    int i,j,k;
+    for(i=1; i<width-1; i++){
+        for(j=1; j<height-1; j++){
+            for(k=1; k<depth-1; k++){
+                if(hostOutputData[((i)*width + (j)) * depth + (k)] != hostInputData[((i-1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j-1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k-1)]
+                + hostInputData[((i+1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j+1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k+1)]
+                - 6*hostInputData[((i)*width + (j)) * depth + (k)])
+                printf("Failed at %d,%d,%d : Found:%f ; Expected:%f\n",i,j,k,hostOutputData[((i)*width + (j)) * depth + (k)], hostInputData[((i-1)*width + (j)) * depth + (k)] 
+                + hostInputData[((i)*width + (j-1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k-1)]
+                + hostInputData[((i+1)*width + (j)) * depth + (k)] + hostInputData[((i)*width + (j+1)) * depth + (k)] + hostInputData[((i)*width + (j)) * depth + (k+1)]
+                - 6*hostInputData[((i)*width + (j)) * depth + (k)]);
+            }
+        }
+    }*/
 
     wbSolution(arg, output);
 
