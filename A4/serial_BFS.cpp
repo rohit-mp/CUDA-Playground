120ce33da86f41a57a26670bc1d800f1847bbc91
Rohit M P <rohitmp99@gmail.com>
2018-12-16 00:06:15 +0530

    Modified the I/O format

--- A4/serial_BFS.cpp
+++ A4/serial_BFS.cpp
@@ -27,11 +27,12 @@ int main(int argc, char *argv[]){
     //generating an array of cumulative number of edges from vertices
     int *cum_no = (int*)calloc(nodes,sizeof(int));
     input >> i; //ignore 0
-    for(i=0; i<nodes; i++){
+    for(i=0; i<nod
+    es; i++){
         input >> cum_no[i];
     }
     printf("generated cum_no array\n");
-    //generating the graph in vector form
+
 	vector<vector<int> > adj(nodes, vector<int>());
     int n=0;
     for(i=0; i<edges*2; i++){
@@ -42,13 +43,6 @@ int main(int argc, char *argv[]){
             n++;
     }
     printf("Generated adj\n");
-    /*for(n=0; n<nodes; n++){
-        printf("%d -> ",n);
-        for(i=0; i<adj[n].size(); i++){
-            printf("%d, ",adj[n][i]);
-        }
-        printf("\n");
-    }*/
 
     //performing BFS
     int depth = 0;
@@ -59,14 +53,14 @@ int main(int argc, char *argv[]){
     q.push(0);
     while(!q.empty()){
         int cur = q.front();
-        //printf("Popping %d\n",cur);
         q.pop();
+        
         for(i=0; i<adj[cur].size(); i++){
             if( !visited[adj[cur][i]] ){
                 d[adj[cur][i]] = d[cur] + 1;
                 visited[adj[cur][i]] = 1;
                 q.push(adj[cur][i]);
-                //printf("Pushing %d\n",adj[cur][i]);
+                
                 if(depth < d[adj[cur][i]]){
                     depth = d[adj[cur][i]];
                 }
@@ -74,8 +68,10 @@ int main(int argc, char *argv[]){
         }
     }
 
-    for(i=0; i<nodes; i++){
-        printf("Depth of node %d is %d\n",i,d[i]);
-    }
     cout << "Depth of the tree is " << depth << endl;
+
+    freopen("output", "w", stdout);
+    for(i=0; i<nodes; i++){
+        cout << d[i] << endl;
+    }
 }
